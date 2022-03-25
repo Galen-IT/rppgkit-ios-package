095a520356a930a064dcc5703f0417b13e9812de
Ivan O <ioparin@pm.me>
2022-03-25 11:46:50 +0400

    init

--- Package.swift
+++ Package.swift
@@ -5,22 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "galenit-package-rppg-sample",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "galenit-package-rppg-sample",
             targets: ["galenit-package-rppg-sample"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:Galen-IT/Galenit.XP.Core_iOS.git", revision: "3d8301f1b3a1e58b6421e654e02327c393c21a80")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "galenit-package-rppg-sample",
-            dependencies: ["GalenitRPPG"]
+            dependencies: [
+                "GalenitRPPG",
+                .product(name: "galenit_device_api", package: "Galenit.XP.Core_iOS")
+            ]
         ),
         .binaryTarget(
             name: "GalenitRPPG",
