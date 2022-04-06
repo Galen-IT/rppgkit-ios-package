cc8eb044353e68583822c2c2f1f0990e89c04a3b
Ivan O <ioparin@pm.me>
2022-04-06 14:54:53 +0400

    improving package manifest to support local deps

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.6
-
+// MARK: - Package Description
 import PackageDescription
 
+// ⚠️ ensure this flag is 'false' before commiting
+let localDependencies = false
+
+// MARK: - Package Configuration
 let package = Package(
     name: "galenit-rppg",
     platforms: [.iOS(.v14)],
@@ -10,24 +14,11 @@ let package = Package(
             name: "GalenitRppgKit",
             targets: ["GalenitRppgKit"]),
     ],
-    dependencies: [
-        .package(
-            url: "git@github.com:galen-it/xpcore-ios-package.git",
-            from: "1.0.1"
-        ),
-        .package(
-            url: "git@github.com:galen-it/binah-ios-package.git",
-            from: "1.0.2"
-        )
-    ],
+    dependencies: localDependencies ? Package.localDeps : Package.remoteDeps,
     targets: [
         .target(
             name: "GalenitRppgKit",
-            dependencies: [
-                "GalenitRPPG",
-                .product(name: "GalenitXpCore", package: "xpcore-ios-package"),
-                .product(name: "GalenitBinah", package: "binah-ios-package")
-            ]
+            dependencies: Package.galenitRppgKitDependencies
         ),
         .binaryTarget(
             name: "GalenitRPPG",
@@ -36,3 +27,28 @@ let package = Package(
         )
     ]
 )
+
+// MARK: -- Dependencies
+extension Package {
+    static var remoteDeps: [Package.Dependency] {
+        [
+            .package(url: "git@github.com:Galen-IT/xpcore-ios-package.git", from: "1.0.1"),
+            .package(url: "git@github.com:Galen-IT/binah-ios-package.git", from: "1.0.2"),
+        ]
+    }
+
+    static var localDeps: [Package.Dependency] {
+        [
+            .package(path: "../xpcore-ios-package"),
+            .package(path: "../binah-ios-package"),
+        ]
+    }
+    
+    static var galenitRppgKitDependencies: [Target.Dependency] {
+        [
+            "GalenitRPPG",
+            .product(name: "GalenitXpCore", package: "xpcore-ios-package"),
+            .product(name: "GalenitBinah", package: "binah-ios-package")
+        ]
+    }
+}
